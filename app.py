c22ffdce7e0d7ef7d4100cd9390884b97c1ba8dd
saklin <saklinmostak321@gmail.com>
2025-06-14 00:12:54 +0530

    several edits made

--- app.py
+++ app.py
@@ -15,8 +15,6 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 app=Flask(__name__)
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
 
 
 app.secret_key=os.getenv("SECRET_KEY")
--- start.sh
+++ start.sh
@@ -1,18 +1,14 @@
-#!/bin/bash
 
 echo "🔽 Downloading ML models..."
 
-# Replace these with your actual file IDs
-PKL_FILE_ID=your_pkl_file_id_here
-H5_FILE_ID=your_h5_file_id_here
-
-# Download model.pkl
+# Download crop_model.pkl from Google Drive
 curl -L -o crop_model.pkl "https://drive.google.com/uc?export=download&id=1Ab3XaEW7NL50Jugt4fPx1dN1eZHrYEQK"
 
-# Download model.h5
+# Download crop_disease_detection.h5 from Google Drive
 curl -L -o crop_disease_detection.h5 "https://drive.google.com/uc?export=download&id=17-Arqf2U0jhQbDjDroQ0GT2EYkUFBpGN"
 
 echo "✅ Models downloaded!"
 
 echo "🚀 Starting the app..."
-gunicorn app:app --bind=0.0.0.0:$PORT # Replace 'app:app' with your actual app entry point if different
+exec gunicorn app:app --bind=0.0.0.0:$PORT
+
